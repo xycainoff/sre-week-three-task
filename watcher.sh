d2d2c9f611843613d79a0ab82a1586b92a6b16e9
xycainoff <88825089+xycainoff@users.noreply.github.com>
2024-04-21 22:14:22 +0000

    implemented scaling down faulty deployment in watcher.sh script

--- watcher.sh
+++ watcher.sh
@@ -1,1 +1,14 @@
-#TODO
+#!/bin/env bash
+NS="sre"
+DEPL_NAME="swype-app"
+MAX_RESTART=3
+while true; do
+    RESTARTS="$(kubectl get pods --namespace $NS --selector=app=$DEPL_NAME | awk '{print $4}' | tail -1)"
+    echo "Pod in $DEPL_NAME deployment restarted $RESTARTS times"
+    if [[ $RESTARTS > $MAX_RESTART ]]; then
+        echo "Too many restarts, scaling down this deployment..."
+        kubectl scale deployment --namespace $NS $DEPL_NAME --replicas=0
+        break
+    fi
+    sleep 60
+done
